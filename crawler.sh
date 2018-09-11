66e264f5ea34c328b81869e736e56cbe3c1664cb
aboyd105205 <aboyd52501@gmail.com>
2018-09-10 20:18:01 -0500

    Added WIP function getLinks() for extracting hyperlinks from a page

--- crawler.sh
+++ crawler.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
-#grep "<a.*>.*</a>" -o index.html | grep "href=\".*\"" -o | grep "\".*\"" -o
+DIR=data
 
-echo $1
+if [[ ! -d "$DIR" ]]; then
+	mkdir $DIR
+fi
+
+cd "$DIR"
+
+getLinks() {
+	wget -O temp1 $1
+	rm temp2
+	grep "<a.*>.*</a>" temp1 | grep -o "href=\"\S*\"" | grep -o "\".*\"" >> temp2
+
+}
+
+getLinks $1
