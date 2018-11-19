4b8f8c6abb27063ae30b6309fe64f66ebcd21ec4
Ashton Boyd <aboyd52501@gmail.com>
2018-11-19 14:58:37 -0600

    added .gitignore, moving visualization towards using D3.

--- visualization/graph.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-import glob
-import networkx as nx
-
-for filename in glob.iglob('../**/*.links', recursive=True):
-    #print( filename )
-    # TODO: translate filename into file object, construct nodes and edges and their data from .links files
